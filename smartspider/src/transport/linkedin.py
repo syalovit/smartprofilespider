73b0b977e35554ecc7d45c0c755ade8a5761dc2a
Steve Yalovitser <syalovitser@yahoo.com>
2014-12-19 21:01:03 -0500

    sweep, first spider

--- smartspider/src/transport/linkedin.py
+++ smartspider/src/transport/linkedin.py
@@ -3,3 +3,18 @@ Created on Dec 19, 2014
 
 @author: Eloise
 '''
+from BeautifulSoup import BeautifulSoup,NavigableString
+import mechanize
+import gzip
+import StringIO
+BR = mechanize.Browser()
+BR.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0'),
+('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+('Accept-Encoding' ,'gzip, deflate'),
+('Accept-Language', 'en-US,en;q=0.5'),
+('Cache-Control', 'max-age=0'),
+('Connection', 'keep-alive')]
+z=BR.open("https://www.linkedin.com/in/frankdgreco")
+y=gzip.GzipFile(fileobj=StringIO.StringIO(buffer(z.get_data())),compresslevel=9)
+parsed = BeautifulSoup(y.read())
+print parsed.text
